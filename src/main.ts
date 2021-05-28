3a602280306368ce12df069a75fd6eb70c9aada5
Marius Ciubotariu <marius.ciubotariu@gmail.com>
2021-05-28 23:11:11 +0100

    remove husky

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,26 @@
 // client.js is used to introduce the reader to generating clients from IDLs.
 // It is not expected users directly test with this example. For a more
 // ergonomic example, see `tests/basic-0.js` in this workspace.
-import * as anchor from "@project-serum/anchor";
-import { publicKeyLayout } from "@project-serum/serum/lib/layout";
-import {
-  Connection,
-  PublicKey,
-  SystemProgram,
-  TransactionInstruction,
-} from "@solana/web3.js";
-import * as fs from "fs";
-import * as utils from "./utils";
+import * as anchor from '@project-serum/anchor';
+import {publicKeyLayout} from '@project-serum/serum/lib/layout';
+import {Connection, PublicKey, SystemProgram, TransactionInstruction,} from '@solana/web3.js';
+import * as fs from 'fs';
+import * as utils from './utils';
 
 /// Accounts
 const liquidityCurrencyMintPubkey =
-  "8PsNqXkQWjxX51dRezzEY8XmzgKbKzG8GXFcQSGEG9xs";
+    '8PsNqXkQWjxX51dRezzEY8XmzgKbKzG8GXFcQSGEG9xs';
 const liquidityCurrencyMint = new PublicKey(liquidityCurrencyMintPubkey);
-const splTokenProgramId = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
+const splTokenProgramId = 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA';
 
 /// After initialization
-const lendingMarketPubkey = "9GLFTCJV1nevb5QKYsCZvAUtxZoEvVLMRVzrHPoDyJK9";
+const lendingMarketPubkey = '9GLFTCJV1nevb5QKYsCZvAUtxZoEvVLMRVzrHPoDyJK9';
 const lendingMarketSecret = Uint8Array.from([
-  198, 88, 4, 216, 87, 98, 235, 163, 167, 71, 56, 180, 32, 209, 7, 42, 149, 21,
-  218, 245, 105, 98, 32, 133, 17, 251, 255, 102, 212, 210, 175, 176, 122, 202,
-  248, 255, 125, 134, 60, 160, 62, 241, 152, 158, 18, 104, 59, 125, 71, 136, 18,
-  96, 12, 152, 80, 146, 54, 66, 196, 186, 70, 175, 152, 64,
+  198, 88,  4,   216, 87,  98,  235, 163, 167, 71,  56,  180, 32,
+  209, 7,   42,  149, 21,  218, 245, 105, 98,  32,  133, 17,  251,
+  255, 102, 212, 210, 175, 176, 122, 202, 248, 255, 125, 134, 60,
+  160, 62,  241, 152, 158, 18,  104, 59,  125, 71,  136, 18,  96,
+  12,  152, 80,  146, 54,  66,  196, 186, 70,  175, 152, 64,
 ]);
 
 // LendingMarketAccount {
@@ -36,12 +32,12 @@ const lendingMarketSecret = Uint8Array.from([
 // }
 
 // const anchor = require('@project-serum/anchor');
-const programPublicKey = "UpbA7oUWbQiXyvbkrMtfMF2gZ3W7F6U3jqxXbUvyPrD";
+const programPublicKey = 'UpbA7oUWbQiXyvbkrMtfMF2gZ3W7F6U3jqxXbUvyPrD';
 const programId = new anchor.web3.PublicKey(programPublicKey);
-const idl = JSON.parse(fs.readFileSync("./target/idl/basic_0.json", "utf8"));
+const idl = JSON.parse(fs.readFileSync('./target/idl/basic_0.json', 'utf8'));
 
 // Configure the local cluster.
-const provider = anchor.Provider.local("https://api.devnet.solana.com/");
+const provider = anchor.Provider.local('https://api.devnet.solana.com/');
 anchor.setProvider(provider);
 const program = new anchor.Program(idl, programId);
 
@@ -53,15 +49,14 @@ async function initializeMarket() {
   // anchor.web3.Keypair.fromSecretKey(lendingMarketSecret);
   const lendingMarketAccount = anchor.web3.Keypair.generate();
   const owner = provider.wallet.publicKey;
-  const liquidityCurrencyMint = new anchor.web3.PublicKey(
-    liquidityCurrencyMintPubkey
-  );
+  const liquidityCurrencyMint =
+      new anchor.web3.PublicKey(liquidityCurrencyMintPubkey);
   const splTokenProgramAccount = new anchor.web3.PublicKey(splTokenProgramId);
 
   console.log(`Payer Pubkey ${provider.wallet.publicKey}`);
   console.log(
-    `Lending Market Account Pub ${lendingMarketAccount.publicKey} Secret ${lendingMarketAccount.secretKey}`
-  );
+      `Lending Market Account Pub ${lendingMarketAccount.publicKey} Secret ${
+          lendingMarketAccount.secretKey}`);
 
   const space = 1 + 1 + 32 + 32 + 32;
 
@@ -77,10 +72,9 @@ async function initializeMarket() {
       anchor.web3.SystemProgram.createAccount({
         fromPubkey: provider.wallet.publicKey,
         newAccountPubkey: lendingMarketAccount.publicKey,
-        space: 8 + space, // Add 8 for the account discriminator.
+        space: 8 + space,  // Add 8 for the account discriminator.
         lamports: await provider.connection.getMinimumBalanceForRentExemption(
-          8 + space
-        ),
+            8 + space),
         programId: program.programId,
       }),
     ],
@@ -90,23 +84,15 @@ async function initializeMarket() {
 async function initializeTrove() {
   const owner = provider.wallet.publicKey;
 
-  const associatedTokenAccount = await utils.findAssociatedTokenAddress(
-    owner,
-    liquidityCurrencyMint
-  );
+  const associatedTokenAccount =
+      await utils.findAssociatedTokenAddress(owner, liquidityCurrencyMint);
   const associatedTokenAccountExists = await utils.checkIfAccountExists(
-    provider.connection,
-    associatedTokenAccount
-  );
+      provider.connection, associatedTokenAccount);
 
-  const troveDataAccount = await utils.troveDataPubkey(
-    owner,
-    utils.TROVE_DATA_SEED
-  );
+  const troveDataAccount =
+      await utils.troveDataPubkey(owner, utils.TROVE_DATA_SEED);
   const troveDataAccountExists = await utils.checkIfAccountExists(
-    provider.connection,
-    associatedTokenAccount
-  );
+      provider.connection, associatedTokenAccount);
 
   const space = 8 + 145;
   const authority = new anchor.web3.Account();
@@ -117,24 +103,18 @@ async function initializeTrove() {
     basePubkey: owner,
     seed: utils.TROVE_DATA_SEED,
     lamports: await provider.connection.getMinimumBalanceForRentExemption(
-      space,
-      "singleGossip"
-    ),
+        space, 'singleGossip'),
     space: space,
     programId: programId,
   });
 
   const [ixAssociatedStableCoin, addressAssociatedStableCoin] =
-    await utils.createAssociatedTokenAccountIx(
-      provider.wallet.publicKey,
-      provider.wallet.publicKey,
-      liquidityCurrencyMint
-    );
+      await utils.createAssociatedTokenAccountIx(
+          provider.wallet.publicKey, provider.wallet.publicKey,
+          liquidityCurrencyMint);
 
-  const [ixAssociatedSol, addresAssociatedSol] = await utils.createSolAccount(
-    provider.connection,
-    owner
-  );
+  const [ixAssociatedSol, addresAssociatedSol] =
+      await utils.createSolAccount(provider.connection, owner);
 
   console.log(`Associated Token Account ${associatedTokenAccount}`);
   console.log(`Exists ${troveDataAccountExists}`);
@@ -147,12 +127,8 @@ async function initializeTrove() {
   console.log(`associatedSolAccount ${addresAssociatedSol.publicKey}`);
 
   let ixCreateAssociatedStableCoinAccount = [];
-  if (
-    (await utils.checkIfAccountExists(
-      provider.connection,
-      associatedTokenAccount
-    )) == false
-  ) {
+  if ((await utils.checkIfAccountExists(
+          provider.connection, associatedTokenAccount)) == false) {
     ixCreateAssociatedStableCoinAccount.push(ixAssociatedStableCoin);
   }
 
@@ -176,34 +152,34 @@ async function initializeTrove() {
   });
 }
 async function depositTroveSol() {
-  await program.rpc.depositTroveSol(new anchor.BN(1234), new anchor.BN(1234), {
-    // accounts: {
-    //   troveDataAccount: 12321,
-    // },
-    // instrctions: [
-    //   ...,
-    //   ...
-    // ],
-    // signers: []
-  });
+  await program.rpc.depositTroveSol(
+      new anchor.BN(1234), new anchor.BN(1234),
+      {
+          // accounts: {
+          //   troveDataAccount: 12321,
+          // },
+          // instrctions: [
+          //   ...,
+          //   ...
+          // ],
+          // signers: []
+      });
 }
 
 async function getTroveData() {
   // Fetch the newly created account from the cluster.
   const owner = provider.wallet.publicKey;
 
-  const troveDataAccount = await utils.troveDataPubkey(
-    owner,
-    utils.TROVE_DATA_SEED
-  );
+  const troveDataAccount =
+      await utils.troveDataPubkey(owner, utils.TROVE_DATA_SEED);
 
   const account = await program.account.troveData(troveDataAccount);
 
   console.log(`Trove Data Account ${JSON.stringify(account)}`);
 }
 
-console.log("Running client.");
+console.log('Running client.');
 // initializeMarket().then(() => console.log('Success'));
 // initializeTrove().then(() => console.log('Success'));
-getTroveData().then(() => console.log("Success"));
+getTroveData().then(() => console.log('Success'));
 // depositTroveSol().then(() => console.log('Success'));
--- src/utils.ts
+++ src/utils.ts
@@ -1,68 +1,42 @@
-import * as anchor from "@project-serum/anchor";
-import { TokenInstructions } from "@project-serum/serum";
-import {
-  AccountLayout,
-  ASSOCIATED_TOKEN_PROGRAM_ID,
-  Token,
-  TOKEN_PROGRAM_ID,
-} from "@solana/spl-token";
-import {
-  Account,
-  Connection,
-  Keypair,
-  PublicKey,
-  SystemProgram,
-  SYSVAR_RENT_PUBKEY,
-  TransactionInstruction,
-} from "@solana/web3.js";
+import * as anchor from '@project-serum/anchor';
+import {TokenInstructions} from '@project-serum/serum';
+import {AccountLayout, ASSOCIATED_TOKEN_PROGRAM_ID, Token, TOKEN_PROGRAM_ID,} from '@solana/spl-token';
+import {Account, Connection, Keypair, PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY, TransactionInstruction,} from '@solana/web3.js';
 
-const programPublicKey = "UpbA7oUWbQiXyvbkrMtfMF2gZ3W7F6U3jqxXbUvyPrD";
+const programPublicKey = 'UpbA7oUWbQiXyvbkrMtfMF2gZ3W7F6U3jqxXbUvyPrD';
 const programId = new anchor.web3.PublicKey(programPublicKey);
 
-export const TROVE_DATA_SEED = "trove_data_7";
-const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new PublicKey(
-  "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
-);
+export const TROVE_DATA_SEED = 'trove_data_7';
+const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID =
+    new PublicKey('ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL');
 
-export const WRAPPED_SOL_MINT = new PublicKey(
-  "So11111111111111111111111111111111111111112"
-);
+export const WRAPPED_SOL_MINT =
+    new PublicKey('So11111111111111111111111111111111111111112');
 
 export async function findAssociatedTokenAddress(
-  owner: PublicKey,
-  tokenMintAddress: PublicKey
-): Promise<PublicKey> {
-  return (
-    await PublicKey.findProgramAddress(
+    owner: PublicKey, tokenMintAddress: PublicKey): Promise<PublicKey> {
+  return (await PublicKey.findProgramAddress(
       [
         owner.toBuffer(),
         TOKEN_PROGRAM_ID.toBuffer(),
         tokenMintAddress.toBuffer(),
       ],
-      SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID
-    )
-  )[0];
+      SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID))[0];
 }
 
 export async function checkIfAccountExists(
-  connection: Connection,
-  account: PublicKey
-): Promise<boolean> {
+    connection: Connection, account: PublicKey): Promise<boolean> {
   const acc = await connection.getAccountInfo(account);
   console.log(`Account ${account} exists ${JSON.stringify(acc)}`);
   return acc != null;
 }
 
 export async function createAssociatedTokenAccountIx(
-  fundingAddress: PublicKey,
-  walletAddress: PublicKey,
-  splTokenMintAddress: PublicKey
-) {
-  const associatedTokenAddress = await findAssociatedTokenAddress(
-    walletAddress,
-    splTokenMintAddress
-  );
-  const systemProgramId = new PublicKey("11111111111111111111111111111111");
+    fundingAddress: PublicKey, walletAddress: PublicKey,
+    splTokenMintAddress: PublicKey) {
+  const associatedTokenAddress =
+      await findAssociatedTokenAddress(walletAddress, splTokenMintAddress);
+  const systemProgramId = new PublicKey('11111111111111111111111111111111');
   const keys = [
     {
       pubkey: fundingAddress,
@@ -120,18 +94,15 @@ export async function troveDataPubkey(userPubKey: PublicKey, seed: string) {
 }
 
 export async function createSolAccount(
-  connection: Connection,
-  payer: PublicKey
-): Promise<[TransactionInstruction, Keypair]> {
+    connection: Connection,
+    payer: PublicKey): Promise<[TransactionInstruction, Keypair]> {
   const solAccount = anchor.web3.Keypair.generate();
   const space = 8 + 1;
   const createSolTokenAccountIx = SystemProgram.createAccount({
     programId: programId,
     space: space,
     lamports: await connection.getMinimumBalanceForRentExemption(
-      space,
-      "singleGossip"
-    ),
+        space, 'singleGossip'),
     fromPubkey: payer,
     newAccountPubkey: solAccount.publicKey,
   });
